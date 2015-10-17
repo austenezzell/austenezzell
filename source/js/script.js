4093b8eb8b3c34094c511a372b5dec0589d0f8cb
Austen <austenezzell@gmail.com>
2015-10-17 14:37:17 -0700

    bug fixes and content changes

--- source/js/script.js
+++ source/js/script.js
@@ -6,12 +6,12 @@ var aeApp = aeApp || {};
 
   aeApp.bgColor = function() {
       $.adaptiveBackground.run();
-  }
+  };
 
   aeApp.blog = function(){
     var bg = $('.scene_element').css('background-color');
     if($('body').hasClass('blog-page')){
-      if(bg === 'rgb(0, 0, 0)'){
+      if(bg === 'rgba(0, 0, 0, 0)'){
         $('body').addClass('black-bg');
         $('body').css('background', '#000');
       } else {
@@ -19,7 +19,7 @@ var aeApp = aeApp || {};
         $('body').css('background', '');
       }
     }
-  }
+  };
 
   var numberOfClicks = 0;
   var numberOfImages;
@@ -31,14 +31,11 @@ var aeApp = aeApp || {};
      duration: 400,
      delay: 800,
      display: 'block',
-   })
+   });
 
    if($('body').hasClass('blog-page')){
      numberOfImages = $('.image').length;
-     numberOfClicks = 0;
      var $blogContainer = $('.blog-container');
-     var nextPage = $blogContainer.data('next-page');
-     var previousPage = $blogContainer.data('previous-page');
      var fadeIn = function(e, order){
        order *= 400;
        e.velocity({
@@ -47,7 +44,7 @@ var aeApp = aeApp || {};
          duration: 400,
          delay: order
        });
-     }
+     };
 
      fadeIn($('.image-1'), 1);
      if(numberOfImages == 2) {
@@ -78,7 +75,7 @@ var aeApp = aeApp || {};
               duration: 1800,
               delay: 400,
               display: 'block'
-            })
+            });
             }
           });
         } else {
@@ -90,7 +87,7 @@ var aeApp = aeApp || {};
             delay: 400
           });
         }
-    }
+    };
     animateIn($('.feature-container-' + randomFeature), 0);
     animateIn($('.page-description'), 1);
     animateIn($('.home-feature-1'),2);
@@ -98,14 +95,14 @@ var aeApp = aeApp || {};
     animateIn($('.home-feature-3'),4);
     animateIn($('.details'),5);
   }
-}
+};
 
 aeApp.bodyClass = function() {
   var pageClassElement = $('.page');
   var pageClass = pageClassElement.data('page-class');
   $('body').attr('class', '');
   $('body').addClass(pageClass);
-}
+};
 
 aeApp.resize = function() {
   var windowHeight = $( window ).outerHeight();
@@ -131,11 +128,14 @@ aeApp.resize = function() {
         }, {
           duration: 400,
           delay: 800
-        })
+        });
       }
   });
 
   var lastScrollTop = 0;
+
+
+
 
   $(window).scroll(function () {
     var scrollTop                = $(window).scrollTop();
@@ -143,19 +143,13 @@ aeApp.resize = function() {
     var footerDistance           = (footerOffset - scrollTop);
     var footerOpacity            = (footerDistance - 200) / -200;
 
-    $('.footer-bg').css('opacity', footerOpacity);
-
     if (footerOpacity > 1){
-      footerOpacity = 1
+      footerOpacity = 1;
     } else if (footerOpacity < 0) {
-      footerOpacity = 0
+      footerOpacity = 0;
     }
 
-    if ($('body').hasClass('home-page')) {
-      if(scrollTop == 0){
-        footerOpacity = 0;
-      }
-    }
+    $('.footer-bg').css('opacity', footerOpacity);
 
     if ($('body').hasClass('home-page')) {
       var pageDescriptionOffset    = $('.page-description').offset().top;
@@ -167,14 +161,15 @@ aeApp.resize = function() {
 
       var featuredImageOpacity   = (pageDescriptionOpacity) + 1;
       if (featuredImageOpacity > 1){
-        featuredImageOpacity = 1
+        featuredImageOpacity = 1;
       } else if (featuredImageOpacity < 0) {
-        featuredImageOpacity = 0
+        featuredImageOpacity = 0;
       }
     }
 
   });
-}
+
+};
 
 aeApp.homepageHoverAnimations = function() {
   var $employer = $('.employer');
@@ -207,7 +202,7 @@ aeApp.homepageHoverAnimations = function() {
       display: 'none'
     });
   });
-}
+};
 
 
 
@@ -307,7 +302,178 @@ aeApp.recentWorkAnimations = function() {
       });
     }
   });
-}
+};
+
+
+
+
+aeApp.cloud = function() {
+  if($('body').hasClass('nextbit')){
+  (function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+      window.cancelRequestAnimationFrame = window[vendors[x] +
+        'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame)
+      window.requestAnimationFrame = function(callback, element) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function() {
+            callback(currTime + timeToCall);
+          },
+          timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+      };
+
+    if (!window.cancelAnimationFrame)
+      window.cancelAnimationFrame = function(id) {
+        clearTimeout(id);
+      };
+  }());
+
+  var layers = [],
+    objects = [],
+    world = document.getElementById('world'),
+    viewport = document.getElementById('viewport'),
+    d = 0,
+    p = 400,
+    worldXAngle = 0,
+    worldYAngle = 0;
+
+  viewport.style.webkitPerspective = p;
+  viewport.style.MozPerspective = p;
+  viewport.style.oPerspective = p;
+
+  generate();
+
+  function createCloud() {
+    var div = document.createElement('div');
+    div.className = 'cloudBase';
+    var x = 0;
+    var y = 0;
+    var z = 100;
+    var t = 'translateX( ' + x + 'px ) translateY( ' + y + 'px ) translateZ( ' + z + 'px )';
+    world.appendChild(div);
+    for (var j = 0; j < Math.random() * (7 - 4) + 4; j++) {
+      var cloud = document.createElement('div');
+      cloud.style.opacity = 0;
+      cloud.style.opacity = 0.8;
+      cloud.className = 'cloudLayer';
+      if (j === 0) {
+        x = 200;
+      } else if (j === 1) {
+        x = -200;
+      } else {
+        x = Math.random() * (200 - (-200)) + -200;
+      }
+      y = -64;
+      z = Math.random() * (200 - 150) + 150;
+      var a = Math.random() * 360;
+      var s = Math.random() * (1.25 - 0.8) + 0.8;
+      x *= 0.5;
+      y *= 0.5;
+      cloud.data = {
+        x: x,
+        y: y,
+        z: z,
+        a: a,
+        s: s,
+        speed: Math.random() * (0.03 - 0.062) + 0.062
+      };
+      t = 'translateX( ' + x + 'px ) translateY( ' + y + 'px ) translateZ( ' + z + 'px ) rotateZ( ' + a + 'deg ) scale( ' + s + ' )';
+      cloud.style.webkitTransform = t;
+      cloud.style.MozTransform = t;
+      cloud.style.oTransform = t;
+      div.appendChild(cloud);
+      layers.push(cloud);
+    }
+
+    return div;
+  }
+
+  if (window.DeviceMotionEvent) {
+    window.ondevicemotion = function(event) {
+      worldXAngle = event.accelerationIncludingGravity.y / 5;
+      worldYAngle = event.accelerationIncludingGravity.x / 5;
+      updateView();
+    };
+  }
+
+  var initalLocationY;
+  var initalLocationX;
+
+  $('body').on('mouseleave', function(e) {
+    initalLocationY = null;
+    initalLocationX = null;
+  });
+
+  $('body').on('mousemove', function(e) {
+
+    worldYAngle = -(0.5 - (e.clientX / viewport.clientWidth)) * 180 / 100;
+    worldXAngle = (0.5 - (e.clientY / viewport.clientWidth)) * 180 / 100;
+
+    if (!initalLocationY && !initalLocationX) {
+      initalLocationY = worldYAngle;
+      initalLocationX = worldXAngle;
+    }
+
+    worldXAngle = initalLocationX - worldXAngle;
+    worldYAngle = initalLocationY - worldYAngle;
+    updateView();
+
+  });
+
+  function generate() {
+    objects = [];
+    if (world.hasChildNodes()) {
+      while (world.childNodes.length >= 1) {
+        world.removeChild(world.firstChild);
+      }
+    }
+    for (var j = 0; j < 1; j++) {
+      objects.push(createCloud());
+    }
+  }
+
+  function updateView() {
+    var t = 'rotateX( ' + worldXAngle + 'deg) rotateY( ' + worldYAngle + 'deg)';
+    world.style.webkitTransform = t;
+    world.style.MozTransform = t;
+    world.style.oTransform = t;
+  }
+
+  function onContainerMouseWheel(event) {
+    event = event ? event : window.event;
+    d = d - (event.detail ? event.detail * -5 : event.wheelDelta / 8);
+    updateView();
+  }
+
+  function update() {
+
+    for (var j = 0; j < layers.length; j++) {
+      var layer = layers[j];
+      layer.data.a += layer.data.speed;
+      var t = 'translateX( ' + layer.data.x + 'px ) translateY( ' + layer.data.y + 'px ) translateZ( ' + layer.data.z + 'px ) rotateY( ' + (-worldYAngle) + 'deg ) rotateX( ' + (-worldXAngle) + 'deg ) rotateZ( ' + layer.data.a + 'deg ) scale( ' + layer.data.s + ')';
+      layer.style.webkitTransform = t;
+      layer.style.MozTransform = t;
+      layer.style.oTransform = t;
+    }
+
+    requestAnimationFrame(update);
+
+  }
+
+  update();
+  }
+};
+
+
+
+
 
 aeApp.smoothState = function() {
   // SmoothState
@@ -331,11 +497,27 @@ aeApp.smoothState = function() {
               duration: 400,
               delay: 0
           });
-        } else if ($(targetElement).hasClass('next') || $(targetElement).hasClass('featured-case-study') || $(targetElement).hasClass('blog-container')) {
-          var projectColor = $(targetElement).attr("data-background");
+
+        } else if ($(targetElement).hasClass('nav-home')){
+          $('html').velocity("scroll", { offset: '0px', mobileHA: false });
+          $('body').velocity({
+              backgroundColor: '#fff'
+            }, {
+              duration: 400,
+              delay: 0
+          });
+        } else if ($(targetElement).hasClass('nav-blog') || $(targetElement).hasClass('nextBlogEntry')){
+          $('body').velocity({
+              backgroundColor: '#000'
+            }, {
+              duration: 400,
+              delay: 0
+          });
+        } else if ($(targetElement).hasClass('next') || $(targetElement).hasClass('featured-case-study')) {
+          var nextProjectColor = $(targetElement).attr("data-background");
 
           $('body').velocity({
-              backgroundColor: projectColor
+              backgroundColor: nextProjectColor
             }, {
               duration: 0,
               delay: 0
@@ -357,7 +539,7 @@ aeApp.smoothState = function() {
           $container.addClass('is-exiting');
           // Restart your animation
           smoothState.restartCSSAnimations();
-          aeApp.onload();
+          // aeApp.onload();
         }
       },
       onReady: {
@@ -372,7 +554,7 @@ aeApp.smoothState = function() {
       }
     },
     smoothState = $('#main-container').smoothState(options).data('smoothState');
-}
+};
 
 $( window ).resize(function() {
   aeApp.resize();
@@ -389,6 +571,7 @@ aeApp.onload = function() {
   aeApp.recentWorkAnimations();
   aeApp.homepageHoverAnimations();
   aeApp.hoverMovement();
+  aeApp.cloud();
 };
 
 (function($, window, document) {
